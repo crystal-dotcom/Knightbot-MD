588dece55dd63218ca310e45d0dc045f34609e11
crystal-dotcom <lovexsty@gmail.com>
2025-04-21 16:35:38 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "knight Bot",
+  botOwner: 'Crissy', // your name
+  ownerNumber: '7059123108', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
