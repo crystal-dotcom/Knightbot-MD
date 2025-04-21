53a65463d29f283cc2bd3ebb4b5221a3f30aeacb
crystal-dotcom <lovexsty@gmail.com>
2025-04-21 18:09:08 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "knight Bot",
+  botName: "Crissy Bot",
   botOwner: 'Crissy', // your name
   ownerNumber: '7059123108', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "Private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
